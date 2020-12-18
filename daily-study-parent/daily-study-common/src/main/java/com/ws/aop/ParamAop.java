53975beb1e5981079ce0363efad72ef8765fdf70
wangshuo <wangshuo@yunzhangfang.com>
2020-12-18 18:21:18 +0800

    日常-模块提交

---
+++ daily-study-parent/daily-study-common/src/main/java/com/ws/aop/ParamAop.java
@@ -0,0 +1,15 @@
+package com.ws.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: wangshuo
+ * @Date: 2020/12/18 16:59
+ */
+@Component
+@Aspect
+@Slf4j
+public class ParamAop {
+}
---
+++ daily-study-parent/daily-study-storage/src/main/java/com/ws/service/Test.java
@@ -0,0 +1,8 @@
+package com.ws.service;
+
+/**
+ * @Author: wangshuo
+ * @Date: 2020/12/18 18:20
+ */
+public class Test {
+}
