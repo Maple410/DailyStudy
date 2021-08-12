ce4fed0e73fd2d8b418ede8a09adf82242e9b91a
wangshuo <wangshuo@yunzhangfang.com>
2021-08-12 17:39:13 +0800

    EventBus 事件总线

---
+++ daily-study-parent/daily-study-service/src/main/java/com/ws/config/EventBusConfigBean.java
@@ -0,0 +1,35 @@
+package com.ws.config;
+
+import com.google.common.eventbus.AsyncEventBus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+/**
+ * @Author: wangshuo
+ * @Date: 2021/8/12 14:12
+ *
+ * https://blog.csdn.net/qq_38345296/article/details/100539989
+ */
+@Configuration
+public class EventBusConfigBean {
+
+    @Bean
+    @Scope("singleton")
+    public AsyncEventBus asyncEventBus() {
+        final ThreadPoolTaskExecutor executor = executor();
+        return new AsyncEventBus(executor);
+    }
+
+    @Bean
+    public ThreadPoolTaskExecutor executor(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(50);
+        return executor;
+    }
+
+
+}
---
+++ daily-study-parent/daily-study-web/src/main/java/com/ws/controller/EventBusController.java
@@ -0,0 +1,30 @@
+package com.ws.controller;
+
+import com.google.common.eventbus.AsyncEventBus;
+import com.ws.domains.AjaxResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: wangshuo
+ * @Date: 2021/8/12 16:59
+ */
+
+@RestController
+@RequestMapping("/api/eventBus")
+public class EventBusController {
+
+
+    @Autowired
+    private AsyncEventBus asyncEventBus;
+
+
+    @GetMapping("/demo")
+    public AjaxResult demo(@RequestParam String demo) {
+        asyncEventBus.post(demo);
+        return new AjaxResult().success();
+    }
+}
